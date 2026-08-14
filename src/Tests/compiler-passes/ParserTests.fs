8e609afcc79724842f54a13188ce9e92f23ac932
Paul Biggar <paul.biggar@gmail.com>
2026-08-14 11:00:51 -0500

    Speed up grouped parameter parsing

--- src/Tests/compiler-passes/ParserTests.fs
+++ src/Tests/compiler-passes/ParserTests.fs
@@ -17,6 +17,15 @@ let private testInterpolatedExpressionsKeepLexerTerminator () : TestResult =
     | Ok tokens -> Error $"Unexpected interpolated string tokens: {tokens}"
     | Error err -> Error err
 
+let private testFlattenParamGroupsRestoresSourceOrder () : TestResult =
+    let groupsRev =
+        [[("third", AST.TString)]; [("second", AST.TBool)]; [("first", AST.TInt64)]]
+
+    match Parser.flattenParamGroups groupsRev with
+    | [("first", AST.TInt64); ("second", AST.TBool); ("third", AST.TString)] -> Ok ()
+    | parameters -> Error $"Expected parameter groups in source order, got {parameters}"
+
 let tests : (string * (unit -> TestResult)) list = [
     ("Interpolated expressions retain lexer terminator", testInterpolatedExpressionsKeepLexerTerminator)
+    ("Flattened parameter groups retain source order", testFlattenParamGroupsRestoresSourceOrder)
 ]
