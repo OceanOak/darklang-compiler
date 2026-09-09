5acb0a558ae3a4995814280169b15f44e75807ab
Paul Biggar <paul.biggar@gmail.com>
2026-09-09 05:35:21 +0000

    Streamline call-graph reachability

--- src/DarkCompiler/CallGraphReachability.fs
+++ src/DarkCompiler/CallGraphReachability.fs
@@ -7,18 +7,19 @@ module CallGraphReachability
 
 /// Compute the transitive closure of reachable nodes in a call graph.
 let findReachable (callGraph: Map<string, Set<string>>) (roots: Set<string>) : Set<string> =
-    let rec visit visited toVisit =
-        if Set.isEmpty toVisit then
-            visited
-        else
-            let name = Set.minElement toVisit
-            let toVisit' = Set.remove name toVisit
-            if Set.contains name visited then
-                visit visited toVisit'
-            else
-                let visited' = Set.add name visited
-                let calls = Map.tryFind name callGraph |> Option.defaultValue Set.empty
-                let toVisit'' = Set.union toVisit' (Set.difference calls visited')
-                visit visited' toVisit''
+    let rec visit reachable toVisit =
+        match toVisit with
+        | [] -> reachable
+        | name :: rest ->
+            let calls = Map.tryFind name callGraph |> Option.defaultValue Set.empty
+            let (reachable', toVisit') =
+                calls
+                |> Set.fold (fun (known, pending) calledName ->
+                    if Set.contains calledName known then
+                        (known, pending)
+                    else
+                        (Set.add calledName known, calledName :: pending)
+                ) (reachable, rest)
+            visit reachable' toVisit'
 
-    visit Set.empty roots
+    visit roots (Set.toList roots)
